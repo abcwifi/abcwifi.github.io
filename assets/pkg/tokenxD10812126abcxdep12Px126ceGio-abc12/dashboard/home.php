1a5fb2598535a61f85a0b0268d9de993526a53f6
abcwifi <akacrkwijaya@gmail.com>
2025-07-02 03:50:48 +0700

    Add files via upload

---
+++ assets/pkg/tokenxD10812126abcxdep12Px126ceGio-abc12/dashboard/home.php
@@ -0,0 +1,335 @@
+<?php
+/*
+ *  Copyright (C) 2018 Laksamadi Guko.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+session_start();
+// hide all error
+error_reporting(0);
+if (!isset($_SESSION["mikhmon"])) {
+  header("Location:../dealer.php?id=login");
+} else {
+
+
+// get MikroTik system clock
+  $getclock = $API->comm("/system/clock/print");
+  $clock = $getclock[0];
+  $timezone = $getclock[0]['time-zone-name'];
+  $_SESSION['timezone'] = $timezone;
+  date_default_timezone_set($timezone);
+
+// get system resource MikroTik
+  $getresource = $API->comm("/system/resource/print");
+  $resource = $getresource[0];
+
+// get routeboard info
+  $getrouterboard = $API->comm("/system/routerboard/print");
+  $routerboard = $getrouterboard[0];
+/*
+// move hotspot log to disk *
+  $getlogging = $API->comm("/system/logging/print", array("?prefix" => "->", ));
+  $logging = $getlogging[0];
+  if ($logging['prefix'] == "->") {
+  } else {
+    $API->comm("/system/logging/add", array("action" => "disk", "prefix" => "->", "topics" => "hotspot,info,debug", ));
+  }
+
+// get hotspot log
+  $getlog = $API->comm("/log/print", array("?topics" => "hotspot,info,debug", ));
+  $log = array_reverse($getlog);
+  $THotspotLog = count($getlog);
+*/
+// get & counting hotspot users
+  $countallusers = $API->comm("/ip/hotspot/user/print", array("count-only" => ""));
+  if ($countallusers < 2) {
+    $uunit = "item";
+  } elseif ($countallusers > 1) {
+    $uunit = "items";
+  }
+
+// get & counting hotspot active
+  $counthotspotactive = $API->comm("/ip/hotspot/active/print", array("count-only" => ""));
+  if ($counthotspotactive < 2) {
+    $hunit = "item";
+  } elseif ($counthotspotactive > 1) {
+    $hunit = "items";
+  }
+
+  if ($livereport == "disable") {
+    $logh = "457px";
+    $lreport = "style='display:none;'";
+  } else {
+    $logh = "350px";
+    $lreport = "style='display:block;'";
+  }
+/*
+// get selling report
+    $thisD = date("d");
+    $thisM = strtolower(date("M"));
+    $thisY = date("Y");
+
+    if (strlen($thisD) == 1) {
+      $thisD = "0" . $thisD;
+    } else {
+      $thisD = $thisD;
+    }
+
+    $idhr = $thisM . "/" . $thisD . "/" . $thisY;
+    $idbl = $thisM . $thisY;
+
+    $getSRHr = $API->comm("/system/script/print", array(
+      "?source" => "$idhr",
+    ));
+    $TotalRHr = count($getSRHr);
+    $getSRBl = $API->comm("/system/script/print", array(
+      "?owner" => "$idbl",
+    ));
+    $TotalRBl = count($getSRBl);
+
+    for ($i = 0; $i < $TotalRHr; $i++) {
+
+      $tHr += explode("-|-", $getSRHr[$i]['name'])[3];
+
+    }
+    for ($i = 0; $i < $TotalRBl; $i++) {
+
+      $tBl += explode("-|-", $getSRBl[$i]['name'])[3];
+    }
+  }*/
+}
+?>
+<?php
+/*
+ *  Copyright (C) 2018 Laksamadi Guko.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// hide all error
+error_reporting(0);
+if (!isset($_SESSION["mikhmon"])) {
+  header("Location:../cek.php?id=login");
+} else {
+
+// array color
+  $color = array('1' => 'bg-blue', 'bg-indigo', 'bg-purple', 'bg-pink', 'bg-red', 'bg-yellow', 'bg-green', 'bg-teal', 'bg-cyan', 'bg-grey', 'bg-light-blue');
+
+  if (isset($_POST['save'])) {
+
+    $suseradm = ($_POST['useradm']);
+    $spassadm = encrypt($_POST['passadm']);
+    $logobt = ($_POST['logobt']);
+    $qrbt = ($_POST['qrbt']);
+
+    $cari = array('1' => "mikhmon<|<$useradm", "mikhmon>|>$passadm");
+    $ganti = array('1' => "mikhmon<|<$suseradm", "mikhmon>|>$spassadm");
+
+    for ($i = 1; $i < 3; $i++) {
+      $file = file("./include/config.php");
+      $content = file_get_contents("./include/config.php");
+      $newcontent = str_replace((string)$cari[$i], (string)$ganti[$i], "$content");
+      file_put_contents("./include/config.php", "$newcontent");
+    }
+
+  
+  $gen = '<?php $qrbt="' . $qrbt . '";?>';
+          $key = './include/quickbt.php';
+          $handle = fopen($key, 'w') or die('Cannot open file:  ' . $key);
+          $data = $gen;
+          fwrite($handle, $data);
+    echo "<script>window.location='./dealer.php?id=login'</script>";
+  }
+
+}
+?>
+<script>
+  function Pass(id){
+    var x = document.getElementById(id);
+    if (x.type === 'password') {
+    x.type = 'text';
+    } else {
+    x.type = 'password';
+    }}
+</script>
+
+<script>
+  var _0x7470=["\x68\x6F\x73\x74\x6E\x61\x6D\x65","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x2E","\x73\x70\x6C\x69\x74","\x6D\x69\x6B\x68\x6D\x6F\x6E\x2E\x6F\x6E\x6C\x69\x6E\x65","\x78\x62\x61\x6E\x2E\x78\x79\x7A","\x6C\x6F\x67\x61\x6D\x2E\x69\x64","\x6D\x69\x6E\x69\x73\x2E\x69\x64","\x69\x6E\x64\x65\x78\x4F\x66","\x3C\x73\x70\x61\x6E\x20\x3E\x3C\x69\x20\x63\x6C\x61\x73\x73\x3D\x22\x74\x65\x78\x74\x2D\x77\x68\x69\x74\x65\x20\x66\x61\x20\x66\x61\x2D\x69\x6E\x66\x6F\x2D\x63\x69\x72\x63\x6C\x65\x22\x3E\x3C\x2F\x69\x3E\x20\x3C\x61\x20\x63\x6C\x61\x73\x73\x3D\x22\x74\x65\x78\x74\x2D\x62\x6C\x75\x65\x22\x20\x68\x72\x65\x66\x3D\x22\x2E\x2F\x61\x64\x6D\x69\x6E\x2E\x70\x68\x70\x3F\x69\x64\x3D\x61\x62\x6F\x75\x74\x22\x3E\x43\x68\x65\x63\x6B\x20\x55\x70\x64\x61\x74\x65\x3C\x2F\x61\x3E\x3C\x2F\x73\x70\x61\x6E\x3E","\x68\x74\x6D\x6C","\x23\x6E\x65\x77\x56\x65\x72","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x72\x61\x77\x2E\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6F\x6E\x74\x65\x6E\x74\x2E\x63\x6F\x6D\x2F\x6C\x61\x6B\x73\x61\x31\x39\x2F\x6D\x69\x6B\x68\x6D\x6F\x6E\x76\x33\x2F\x6D\x61\x73\x74\x65\x72\x2F\x76\x65\x72\x73\x6F\x6E\x2E\x74\x78\x74\x3F\x74\x3D","\x72\x61\x6E\x64\x6F\x6D","\x66\x6C\x6F\x6F\x72","\x76","\x76\x65\x72\x73\x69\x6F\x6E","","\x72\x65\x70\x6C\x61\x63\x65","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x6C\x6F\x61\x64\x56","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x20","\x75\x70\x64\x61\x74\x65\x64","\x2D","\x4E\x65\x77\x20\x56\x65\x72\x73\x69\x6F\x6E\x20","\x3C\x62\x72\x3E\x3C\x73\x70\x61\x6E\x20\x3E\x3C\x69\x20\x63\x6C\x61\x73\x73\x3D\x22\x74\x65\x78\x74\x2D\x77\x68\x69\x74\x65\x20\x66\x61\x20\x66\x61\x2D\x69\x6E\x66\x6F\x2D\x63\x69\x72\x63\x6C\x65\x22\x3E\x3C\x2F\x69\x3E\x20\x3C\x61\x20\x63\x6C\x61\x73\x73\x3D\x22\x74\x65\x78\x74\x2D\x62\x6C\x75\x65\x22\x20\x68\x72\x65\x66\x3D\x22\x2E\x2F\x61\x64\x6D\x69\x6E\x2E\x70\x68\x70\x3F\x69\x64\x3D\x61\x62\x6F\x75\x74\x22\x3E\x43\x68\x65\x63\x6B\x20\x55\x70\x64\x61\x74\x65\x3C\x2F\x61\x3E\x3C\x2F\x73\x70\x61\x6E\x3E","\x67\x65\x74\x4A\x53\x4F\x4E"];var hname=window[_0x7470[1]][_0x7470[0]];var dom=hname[_0x7470[3]](_0x7470[2])[1]+ _0x7470[2]+ hname[_0x7470[3]](_0x7470[2])[2];var domArray=[_0x7470[4],_0x7470[5],_0x7470[6],_0x7470[7]];var a=domArray[_0x7470[8]](hname);var b=domArray[_0x7470[8]](dom);if(dom== _0x7470[4]){$(_0x7470[11])[_0x7470[10]](_0x7470[9])}else {if(a> 0|| b> 0){}else {$[_0x7470[27]](_0x7470[12]+ (Math[_0x7470[14]]((Math[_0x7470[13]]()* 999999999)+ 1))* 128,function(_0xc1b4x6){getNewVer= (_0xc1b4x6[_0x7470[16]])[_0x7470[3]](_0x7470[15])[1];var _0xc1b4x7=parseInt(getNewVer[_0x7470[18]](_0x7470[2],_0x7470[17]));var _0xc1b4x8=document[_0x7470[21]](_0x7470[20])[_0x7470[19]];var _0xc1b4x9=(_0xc1b4x8[_0x7470[3]](_0x7470[22])[0])[_0x7470[3]](_0x7470[15])[1];var _0xc1b4xa=parseInt(_0xc1b4x9[_0x7470[18]](_0x7470[2],_0x7470[17]));var _0xc1b4xb=(_0xc1b4x7- _0xc1b4xa);getNewVer= (_0xc1b4x6[_0x7470[16]])[_0x7470[3]](_0x7470[15])[1];var _0xc1b4x7=parseInt(getNewVer[_0x7470[18]](_0x7470[2],_0x7470[17]));var _0xc1b4x8=document[_0x7470[21]](_0x7470[20])[_0x7470[19]];var _0xc1b4x9=(_0xc1b4x8[_0x7470[3]](_0x7470[22])[0])[_0x7470[3]](_0x7470[15])[1];var _0xc1b4xa=parseInt(_0xc1b4x9[_0x7470[18]](_0x7470[2],_0x7470[17]));var _0xc1b4xb=(_0xc1b4x7- _0xc1b4xa);getNewD= (_0xc1b4x6[_0x7470[23]])[_0x7470[3]](_0x7470[22])[0];newD= parseInt((getNewD)[_0x7470[3]](_0x7470[24])[2]+ (getNewD)[_0x7470[3]](_0x7470[24])[0]+ (getNewD)[_0x7470[3]](_0x7470[24])[1]);var _0xc1b4xc=parseInt((_0xc1b4x8[_0x7470[3]](_0x7470[22])[1])[_0x7470[3]](_0x7470[24])[2]+ (_0xc1b4x8[_0x7470[3]](_0x7470[22])[1])[_0x7470[3]](_0x7470[24])[0]+ (_0xc1b4x8[_0x7470[3]](_0x7470[22])[1][_0x7470[3]](_0x7470[24]))[1]);var _0xc1b4xd=(newD- _0xc1b4xc);if(_0xc1b4xb> 0|| _0xc1b4xd> 0){$(_0x7470[11])[_0x7470[10]](_0x7470[25]+ _0xc1b4x6[_0x7470[16]]+ _0x7470[22]+ _0xc1b4x6[_0x7470[23]]+ _0x7470[26])}})}}
+</script>
+
+<div id="reloadHome">
+
+    <div id="r_1" class="row">
+      <div class="col-4">
+        <div class="box bmh-75 box-bordered">
+          <div class="box-group">
+            <div class="box-group-icon"><i class="fa fa-calendar"></i></div>
+              <div class="box-group-area">
+                <span ><?= $_system_date_time ?><br>
+                    <?php 
+                    echo ucfirst($clock['date']) . " " . $clock['time'] . "<br>
+                    ".$_uptime." : " . formatDTM($resource['uptime']);
+                    $_SESSION[$session.'sdate'] = $clock['date'];
+                    ?>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      <div class="col-4">
+        <div class="box bmh-75 box-bordered">
+          <div class="box-group">
+          <div class="box-group-icon"><i class="fa fa-info-circle"></i></div>
+              <div class="box-group-area">
+                <span >
+                    <?php
+                    echo $_board_name." : " . $resource['board-name'] . "<br/>
+                    ".$_model." : " . $routerboard['model'] . "<br/>
+                    Router OS : " . $resource['version'];
+                    ?>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+    <div class="col-4">
+      <div class="box bmh-75 box-bordered">
+        <div class="box-group">
+          <div class="box-group-icon"><i class="fa fa-server"></i></div>
+              <div class="box-group-area">
+                <span >
+                    <?php
+                    echo $_cpu_load." : " . $resource['cpu-load'] . "%<br/>
+                    ".$_free_memory." : " . formatBytes($resource['free-memory'], 2) . "<br/>
+                    ".$_free_hdd." : " . formatBytes($resource['free-hdd-space'], 2)
+                    ?>
+                </span>
+                </div>
+              </div>
+            </div>
+          </div> 
+      </div>
+
+        <div class="row">
+          <div  class="col-8">
+            <div id="r_2"class="row">
+                  <div class="row">
+                    <div class="col-3 col-box-6">
+                      <div class="box bg-blue bmh-75">
+                        <a onclick="cancelPage()" href="./?hotspot=active&session=<?= $session; ?>">
+                          <h1><?= $counthotspotactive; ?>
+                              <span style="font-size: 15px;">Online</span>
+                            </h1>
+                          <div>
+                            <i class="fa fa-laptop"></i> Pengguna Aktif
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                    <div class="col-3 col-box-6">
+                    <div class="box bg-green bmh-75">
+                      <a onclick="cancelPage()" href="./?hotspot=users&profile=all&session=<?= $session; ?>">
+                            <h1><?= $countallusers; ?>
+                              <span style="font-size: 15px;">Voucher</span>
+                            </h1>
+                      <div>
+                            <i class="fa fa-users"></i> Data Paket
+                          </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-3 col-box-6">
+                    <div class="box bg-red bmh-75">
+                      <a onclick="cancelPage()" href="./?report=selling&idbl=<?= strtolower(date("M")) . date("Y"); ?>&session=<?= $session; ?>">
+                           <h1><i class=" fa fa-money"></i>
+                              <span style="font-size: 15px;">Penjualan</span>
+                          </h1>
+                        <div>
+                            <i class=" fa fa-money"></i> Cek & Aktivasi
+                        </div> 
+                    </a>
+                  </div>
+                </div>
+
+                  <div class="col-3 col-box-6">
+                    <div class="box bg-red bmh-75">
+                      <a onclick="cancelPage()" href="./?hotspot=list-quick-print&session=<?= $session; ?>">
+                        <div>
+                          <h1><i class="fa fa-print"></i>
+                              <span style="font-size: 15px;">Cetak Voucher</span>
+                          </h1>
+                        </div>
+                        <div>
+                            <i class="fa fa-user-plus"></i> Transfer & Cetak
+                        </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+           
+          <center>
+          <form autocomplete="off" method="post" action="">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title"><i class="fa fa-user-circle"></i> Akses Kode Token Agen : <?= $useradm; ?></h3>
+              </div>
+            <div class="card-body">
+      <table class="table table-sm">
+        <tr>
+          <td hidden="true" class="align-middle">Kode Seller </td><td><input hidden="true" class="" id="useradm" type="text" size="10" name="useradm"  value="<?= $useradm; ?>" required="0"/></td>
+        </tr>        
+          <tr>
+          <td class="align-middle">Kode Token </td>
+          <td>
+          <div class="input-group">
+          <div class="input-group-11 col-box-10">
+                <input class="group-item group-item-l" id="passadm" type="password" size="10" name="passadm"  value="<?= decrypt($passadm); ?>" required="1"/>
+              </div>
+                <div class="input-group-1 col-box-2">
+                  </div>
+                </div>
+            </div>
+          </td>
+        </tr>
+        
+        <tr>
+          <td></td><td class="text-right">
+              <div class="input-group-4">
+                  <input class="group-item group-item-l" type="submit" style="cursor: pointer;" name="save" value="Simpan"/>
+
+                </div>
+                <div class="input-group-2">
+                  <div style="cursor: pointer;" class="group-item group-item-r pd-2p5 text-center" onclick="location.reload();" title="Reload Data"><i class="fa fa-refresh"></i></div>
+                </div>
+                </div>
+          </td>
+        </tr>
+        
+      </table>    
+</div>
+</div>

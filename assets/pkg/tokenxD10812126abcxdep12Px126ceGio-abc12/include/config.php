fdb6bd3fc0a383bd888a188f62e6aaab86c2f677
abcwifi <akacrkwijaya@gmail.com>
2025-07-02 03:56:40 +0700

    config.php

--- assets/pkg/tokenxD10812126abcxdep12Px126ceGio-abc12/include/config.php
+++ assets/pkg/tokenxD10812126abcxdep12Px126ceGio-abc12/include/config.php
@@ -2,4 +2,4 @@
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mikhmon'] = array ('1'=>'mikhmon<|<100','mikhmon>|>aWhr');
 
-$data['abc11lbs'] = array ('1'=>'abc11lbs!10.8.0.11','abc11lbs@|@print','abc11lbs#|#qKObpqVjamRmbWdzmQ==','abc11lbs%abc','abc11lbs^abc.net','abc11lbs&Rp','abc11lbs*100','abc11lbs(1','abc11lbs)','abc11lbs=5','abc11lbs@!@disable');
+$data['abc11lbs'] = array ('1'=>'abc11lbs!38.47.185.66','abc11lbs@|@print','abc11lbs#|#qKObpqVjamRmbWdzmQ==','abc11lbs%abc','abc11lbs^abc.net','abc11lbs&Rp','abc11lbs*100','abc11lbs(1','abc11lbs)','abc11lbs=5','abc11lbs@!@disable');

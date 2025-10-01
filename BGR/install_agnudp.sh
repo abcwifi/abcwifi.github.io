d6c60f407886efd230fe64fd0536822cf55554cf
abcwifi <akacrkwijaya@gmail.com>
2025-10-02 04:06:26 +0700

    install_agnudp.sh

--- BGR/install_agnudp.sh
+++ BGR/install_agnudp.sh
@@ -756,15 +756,13 @@ perform_remove() {
 setup_ssl() {
     echo "Installing SSL certificates"
 
-    openssl genrsa -out /etc/hysteria/privatekey.pem 2048
-
     openssl genrsa -out /etc/hysteria/hysteria.ca.key 2048
 
     openssl req -new -x509 -days 3650 -key /etc/hysteria/hysteria.ca.key -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=Hysteria Root CA" -out /etc/hysteria/hysteria.ca.crt
 
     openssl req -newkey rsa:2048 -nodes -keyout /etc/hysteria/hysteria.server.key -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=$DOMAIN" -out /etc/hysteria/hysteria.server.csr
 
-    openssl x509 -req -extfile <(printf "subjectAltName=DNS:syahdan.indoserver.de,DNS:syahdan.indoserver.de") -days 3650 -in /etc/hysteria/hysteria.server.csr -CA /etc/hysteria/hysteria.ca.crt -CAkey /etc/hysteria/hysteria.ca.key -CAcreateserial -out /etc/hysteria/hysteria.server.crt
+    openssl x509 -req -extfile <(printf "subjectAltName=DNS:syahdan.indoserver.de,DNS:syahdan.indoserver.de","keyUsage=digitalSignature") -days 3650 -in /etc/hysteria/hysteria.server.csr -CA /etc/hysteria/hysteria.ca.crt -CAkey /etc/hysteria/hysteria.ca.key -CAcreateserial -out /etc/hysteria/hysteria.server.crt
 }
 
 start_services() {

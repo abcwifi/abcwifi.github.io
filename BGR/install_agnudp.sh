34cd0702c45ee655a33d035ea1c807fa50eb2dcc
abcwifi <akacrkwijaya@gmail.com>
2025-11-10 11:36:20 +0700

    install_agnudp.sh

--- BGR/install_agnudp.sh
+++ BGR/install_agnudp.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Domain Name
-DOMAIN="syahdan.indoserver.de"
+DOMAIN="connect.indoserver.de"
 
 # PROTOCOL
 PROTOCOL="udp"
@@ -755,14 +755,22 @@ perform_remove() {
 
 setup_ssl() {
     echo "Installing SSL certificates"
-
-    openssl genrsa -out /etc/hysteria/hysteria.ca.key 2048
-
+	
+    openssl genrsa -out /etc/hysteria/hysteria.ca.key 2048  2048
+	
     openssl req -new -x509 -days 3650 -key /etc/hysteria/hysteria.ca.key -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=Hysteria Root CA" -out /etc/hysteria/hysteria.ca.crt
-
-    openssl req -newkey rsa:2048 -nodes -keyout /etc/hysteria/hysteria.server.key -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=$DOMAIN" -out /etc/hysteria/hysteria.server.csr
-
-    openssl x509 -req -extfile <(printf "subjectAltName=DNS:syahdan.indoserver.de,DNS:syahdan.indoserver.de") -days 3650 -in /etc/hysteria/hysteria.server.csr -CA /etc/hysteria/hysteria.ca.crt -CAkey /etc/hysteria/hysteria.ca.key -CAcreateserial -out /etc/hysteria/hysteria.server.crt
+    
+	openssl req -newkey rsa:2048 -nodes -keyout /etc/hysteria/hysteria.server.key -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=$DOMAIN" -out /etc/hysteria/hysteria.server.csr
+    
+	openssl x509 -req -extfile <(printf "subjectAltName=DNS:connect.indoserver.de,DNS:connect.indoserver.de") -days 3650 -in /etc/hysteria/hysteria.server.csr -CA /etc/hysteria/hysteria.ca.crt -CAkey /etc/hysteria/hysteria.ca.key -CAcreateserial -out /etc/hysteria/hysteria.server.crt
+    
+	openssl genpkey -algorithm RSA -out /etc/hysteria/hysteria.ca.key
+	
+	openssl req -x509 -new -nodes -key /etc/hysteria/hysteria.ca.key -days 3650 -out /etc/hysteria/hysteria.ca.crt -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=Hysteria Root CA"
+    
+	openssl req -newkey rsa:2048 -nodes -keyout /etc/hysteria/hysteria.server.key -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=$DOMAIN" -out /etc/hysteria/hysteria.server.csr
+    
+	openssl x509 -req -extfile <(printf "subjectAltName=DNS:connect.indoserver.de") -days 3650 -in /etc/hysteria/hysteria.server.csr -CA /etc/hysteria/hysteria.ca.crt -CAkey /etc/hysteria/hysteria.ca.key -CAcreateserial -out /etc/hysteria/hysteria.server.crt
 }
 
 start_services() {

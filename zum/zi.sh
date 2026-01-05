0c6947f791e57ea7b1471312365cb372a9153ce6
abcwifi <akacrkwijaya@gmail.com>
2026-01-05 21:06:28 +0700

    zi.sh

--- zum/zi.sh
+++ zum/zi.sh
@@ -106,7 +106,6 @@ After=network.target nss-lookup.target
 [Service]
 Type=simple
 User=root
-Environment=ZIVPN_LOG_LEVEL=info
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
 NoNewPrivileges=true

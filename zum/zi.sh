b4f8d4bd4c1e3a171db71d7576a82765eedd009b
abcwifi <akacrkwijaya@gmail.com>
2026-01-05 20:30:14 +0700

    zi.sh

--- zum/zi.sh
+++ zum/zi.sh
@@ -113,8 +113,6 @@ Environment=ZIVPN_LOG_LEVEL=info
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
 NoNewPrivileges=true
-RemainAfterExit=yes
-
 
 [Install]
 WantedBy=multi-user.target
